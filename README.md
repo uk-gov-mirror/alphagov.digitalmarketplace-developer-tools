f7f11e588def8f2aac415bd46c207a9f96de9a4d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-12-30 11:19:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Digital Marketplace developer tools
+
+Common developer tooling for Digital Marketplace repos.
+
+## Licence
+
+Unless stated otherwise this codebase is released under [the MIT
+License][licence]. This covers both the codebase and any sample code in the
+documentation.
+
+The documentation is [&copy; Crown copyright][copyright] and available under
+the terms of the [Open Government 3.0][ogl] licence.
+
+[licence]: LICENCE
+[copyright]: http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/
+[ogl]: http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/
---
+++ setup.py
@@ -0,0 +1,28 @@
+import re
+import ast
+import setuptools
+
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+
+with open("dmdevtools/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    long_description_content_type = "text/markdown"
+
+setuptools.setup(
+    name="digitalmarketplace-developer-tools",
+    version=version,
+    url="https://github.com/alphagov/digitalmarketplace-developer-tools",
+    license="MIT",
+    author="GDS Developers",
+    description="Common developer tools for Digital Marketplace repos",
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    packages=setuptools.find_packages(),
+    python_requires="~=3.6",
+)
