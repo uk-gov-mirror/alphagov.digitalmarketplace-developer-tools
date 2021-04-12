06817fbeffc22b7f1e6198eaac94619d03818927
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-04-12 12:18:28 +0100

    Add mypy to tests for this repo
    We want to test this code with mypy.
    
    This commit configures mypy to test code in dmdevtools and adds mypy to
    the list of tests that will be run with `invoke test`.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 from dmdevtools.invoke_tasks import library_tasks as ns
 
+# test with mypy
+ns["test"].pre.insert(-1, ns["test-mypy"])
 
 # currently we have no unit tests
 ns["test"].pre.remove(ns["test-python"])
